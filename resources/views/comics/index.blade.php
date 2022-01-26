c297fa837d880c99da33301765868f188b4e1f02
elepot87 <eleonorapotente@gmail.com>
2022-01-26 23:03:24 +0100

    pagina archivio comics

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -6,19 +6,20 @@
 <div class="products">
     <div class="container flex">
         <ul class="container-comics flex no-list-style">
+            @foreach ($comics as $comic)
             <li>
                 <div class="card">
                     <a href="#">
                         <div class="container-img">
-
+                            <img src="{{ $comic->thumb }}" alt="{{ $comic->thumb }}" />
                         </div>
                         <div class="title-comics">
-
+                            {{ $comic->series }}
                         </div>
                     </a>
                 </div>
             </li>
-
+            @endforeach
         </ul>
         <button class="loader">Load more</button>
     </div>
