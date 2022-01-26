0e94a708d40a9a280e4bc78da7e07fa80d878b41
elepot87 <eleonorapotente@gmail.com>
2022-01-26 23:26:59 +0100

    aggiunta pagina dettaglio show con stile e stampa del dato

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -49,7 +49,13 @@ class ComicController extends Controller
      */
     public function show($id)
     {
-        //
+        $comic = Comic::find($id);
+        dump($comic);
+
+        if($comic) {
+            return view('comics.show', compact('comic'));
+        }
+        abort(404);
     }
 
     /**
--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,7 +1,52 @@
 @extends('layouts.main')
 
-@section('head-title', 'Deatail | DC Comics' )
+@section('head-title', 'Detail | DC Comics' )
 
 @section('main-content')
-<h1></h1>
+<section class="jumbotron">
+    <div class="cover">
+        <div class="text">
+            {{$comic->type}}
+        </div>
+        <figure>
+            <img src="{{$comic->thumb}}" alt="{{$comic->title}}">
+        </figure>
+        <div class="cta">
+            <a href="{{route('comics.index')}}">
+                View Gallery
+            </a>
+        </div>
+    </div>
+</section>
+<section class="container-comic">
+    <div class="info">
+        <h1 class="title-comic">{{$comic->title}}</h1>
+        <div class="container-info mt-20">
+            <div class="info-price-availability">
+                <div class="col-sx">
+                    <span class="valuta">U.S. Price:</span>
+                    {{$comic->price}}
+                </div>
+                <div class="col-dx">
+                    <div class="availability">
+                        Available
+                    </div>
+                </div>
+            </div>
+            <div class="check">
+                Check availability
+                <i class="fas fa-caret-down icon-toggle"></i>
+            </div>
+        </div>
+        <div class="description">
+            {{$comic->description}}
+        </div>
+    </div>
+    <div class="banner-adv">
+        <div class="text-adv">advertisement</div>
+        <div class="banner">
+            <img src="{{asset('images/adv.jpg')}}" alt="banner-adv">
+        </div>
+    </div>
+</section>
 @endsection
